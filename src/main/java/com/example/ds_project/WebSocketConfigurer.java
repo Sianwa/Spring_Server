ee2c8ff9ffb0c312dd2c9c897353481b3347ceac
kelsey makale <sianwamak@gmail.com>
2020-07-29 13:08:01 +0300

    Initial commit

---
+++ src/main/java/com/example/ds_project/WebSocketConfigurer.java
@@ -0,0 +1,15 @@
+package com.example.ds_project;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfigurer implements org.springframework.web.socket.config.annotation.WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketHandler(), "/test");
+    }
+}
