32c4a7009383ef1b164ce06f04a234a3ef3f9627
kelsey makale <sianwamak@gmail.com>
2020-09-07 11:52:20 +0300

    added functionality to the handleTextMessage() to sum all the toys in the cart

--- src/main/java/com/example/ds_project/controllers/WebSocketHandler.java
+++ src/main/java/com/example/ds_project/controllers/WebSocketHandler.java
@@ -1,20 +1,29 @@
 package com.example.ds_project.controllers;
-
-import com.google.gson.Gson;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class WebSocketHandler extends TextWebSocketHandler {
-
+    ArrayList<Integer> ToyPricesList = new ArrayList<Integer>();
+    int TotalBill = 0;
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        ArrayList<String> ToyPricesList = new ArrayList<String>();
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        if(session != null) {
+            String ClientMessage = message.getPayload();
+            int payload = Integer.parseInt(ClientMessage);
+            ToyPricesList.add(payload);
 
-        ToyPricesList.add(message.getPayload());
-        System.out.println(ToyPricesList);
+            for (int i = 0; i < ToyPricesList.size(); i++){
+                TotalBill += ToyPricesList.get(i);
+            }
+            System.out.println(ToyPricesList + "Sum total:" + TotalBill);
+            session.sendMessage(new TextMessage(String.valueOf(TotalBill)));
+
+        }
     }
+
 }
